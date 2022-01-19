7dd064040e9965cdd6252d4bd1970b40d610e380
SoniaOrlikowska <sonia.orlikowska@gmail.com>
2022-01-19 21:58:41 +0100

    Adding BFS, without bug

--- BFS.js
+++ BFS.js
@@ -1,81 +1,115 @@
 import DistanceGrid from "./DistanceGrid.js";
 import Grid from "./Grid.js";
 import BinaryTree from "./BinaryTree.js";
+import Distances  from "./Distances.js";
 import Cell from "./Cell.js";
+import AldousBroder from "./AldousBroder.js";
+import "perf_hooks"
 
-class BFS extends DistanceGrid{
+export default class BFS extends DistanceGrid{
     constructor(rows, columns) {
 		super(rows, columns)
-       
-	}
 
+	}
     prepare_stack(startX, startY){
         let stack = [];
         let start = super.get_cell(startX, startY);
         if(stack.length === 0) stack.push(start); //troche głupia metoda a o jak nie jest 0?
         return stack;
     }
-
     // 1. take start cell insert to stack
     // 2. set parents of this cell and visited to yes 
     // 3. pop it form the stack
     // 4. insert its neighbours to stack
     // 5. take last element in stack --> go to 2
     start_bfs_solver(stack){
+        let startTime  = performance.now();
         let start_cell = stack[0];
-        let end = super.get_cell(this.rows - 1, this.columns - 1); //end is fixed as is start
-       
-        start_cell.visited = "yes";
+        //start_cell.visited = "yes";
+        const key = "visited"
+        start_cell[key] = " yes";
+        let end_cell = super.get_cell(this.rows - 1, this.columns - 1); //end is fixed as is start
         let current = start_cell;
-    
-        while(current != end){
-            const cellAdjacents = this.getAdjacents(current);
+        let i = 0; 
+        while(current != end_cell){
+            i++;
+           let cellAdjacents = this.getAdjacents(current);
             let current_id = current.get_id();
-
-            console.log("current id: " + current_id);
             cellAdjacents.forEach(key => {
-                let x = key.charAt(0);
-                let y = key.charAt(2);
+                let array_of_ids = key.split("#",2);
+                const [x, y] = array_of_ids;
+                
                 let adjacentCell = super.get_cell(x,y);
-                if(adjacentCell.visited != "yes"){
-                    adjacentCell.visited = "yes";
+                const cellKey = "visited";
+                if(adjacentCell[cellKey] != "yes"){
+                    adjacentCell[cellKey] = "yes";
                     adjacentCell.parent = current_id;
                     stack.push(adjacentCell);
                 }
             });
+           
             current = stack.pop();
         } 
-        
-       return stack;
+        let endTime = performance.now();
+		let exTime1 = endTime - startTime;
+        return exTime1;
     }
+   
+
+    retreive_bfs_path(start, end){
+        let startTime  = performance.now();
+        let current = end; 
+        let path = [];
+        let parrent_id = null;
+        let parentCell = null;
+        let endTime = 0;
+        
+
+        while(current != start){
+        path.push(current);
+        parrent_id = current.parent;
+        let array_of_ids = parrent_id.split("#",2);
+        const [x, y] = array_of_ids;
+        parentCell = super.get_cell(x,y);
+        super.get_cell(x,y).solution = "yes";
+        current = parentCell; 
+        endTime = performance.now();
+		
+        }
+        let exTime2 = endTime - startTime;
+        return exTime2;
+    }
+      
+
+
 
     getAdjacents(cell){
         return cell.get_links();
     }
-
-    
-
-   
- 
-       //stack.push(start_cell.links[links[0]]);
-      // stack.push(start_cell.links[links[1]]);
-
     }
   
+// let newgrid = new BFS(20,20);
+// let newmaze = new BinaryTree();
+// newmaze.on(newgrid);
+
+// console.log(newgrid.toString());
+// let stack =newgrid.prepare_stack(0,0);
+
+// newgrid.start_bfs_solver(stack);
+// let start = newgrid.get_cell(0,0);
+// let end = newgrid.get_cell(19,19);
+// newgrid.retreive_bfs_path(start, end);
+
+// console.log(newgrid.toString()) 
 
 
-let test = new BFS(3,3);
-let mazeBinary = new BinaryTree();
-mazeBinary.on(test);
-console.log(test.toString());
-let stack = test.prepare_stack(0,0)
-console.log("PRZED");
-console.log(stack.length);
-//console.log(stack[0].get_links());
 
-test.start_bfs_solver(stack);
-console.log("PO");
-console.log(stack.length);
-console.log(stack);
-console.log("FINISH")
-console.log(test.grid);
+
+
+
+
+
+
+
+
+
