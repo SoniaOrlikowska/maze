0c1d0d3535c9d3db8aec8d40663f2b46c788c9b8
SoniaOrlikowska <sonia.orlikowska@gmail.com>
2022-01-19 22:01:44 +0100

    Adding BFS data retriver, changing data columns, using new complexity

--- Solution_demo.js
+++ Solution_demo.js
@@ -1,10 +1,12 @@
 "use strict"
 
 import DistanceGrid from "./DistanceGrid.js"
+
 import BinaryTree from "./BinaryTree.js"
 import Complexity from "./Complexity.js";
 import AldousBroder from "./AldousBroder.js";
 import RecursiveBacktracker from "./RecursiveBacktracker.js";
+import BFS from "./BFS.js"
 import Grid from "./Grid.js"
 import * as fs from 'fs';
 import Cell from "./Cell.js";
@@ -14,7 +16,7 @@ import Cell from "./Cell.js";
 class Solution_demo {
 
   generateMaze(height, width, maze_name) {
-    let distance_grid = new DistanceGrid(height, width);
+    let distance_grid = new BFS(height, width);
     switch (maze_name) {
       case 'Binary':
         let mazeBinary = new BinaryTree();
@@ -34,18 +36,19 @@ class Solution_demo {
     return distance_grid;
   }
 
-  testMethod(){
-    let distance_grid = this.generateMaze(4,4,"Binary");
-    console.log(" " + "\n" + distance_grid);
-    let links = distance_grid.grid[2][2].get_links()
-    console.log(links);
-  }
+  // testMethod(){
+  //   let distance_grid = this.generateMaze(4,4,"Binary");
+  //   console.log(" " + "\n" + distance_grid);
+  //   let links = distance_grid.grid[2][2].get_links()
+  //   console.log(links);
+  // }
 
-  generateDijkstraSolution(distance_grid) {
+  generateDijkstraSolution(distance_grid) { //bardzo niefortuna jest ta nazwa distance grid ale tak na razie bedzie wszedzie
     let start = distance_grid.get_cell(0, 0);
+    let end = distance_grid.get_cell(distance_grid.rows - 1, distance_grid.columns - 1);
     let distances = start.distances();
     distance_grid.distances = distances;
-    let output = distances.path_to(distance_grid.get_cell(distance_grid.rows - 1, distance_grid.columns - 1));
+    let output = distances.path_to(end);
     const [breadcrumbs, executionTime] = output;
     distance_grid.distances = breadcrumbs;
     let totalTime = executionTime.toFixed(4);
@@ -53,11 +56,21 @@ class Solution_demo {
     return totalTime;
   }
 
+  generateBfsSolution(distance_grid){
+    let start = distance_grid.get_cell(0, 0);
+    let end = distance_grid.get_cell(distance_grid.rows - 1, distance_grid.columns - 1);
+    let stack = distance_grid.prepare_stack(0,0);
+    let exTime1 = distance_grid.start_bfs_solver(stack);
+    let exTime2 = distance_grid.retreive_bfs_path(start,end);
+    let totalTime = (exTime1 + exTime2).toFixed(4);
+    return totalTime; 
+  }
 
+  
   calculateComplexity(mazeGrid) {
     let complexity = new Complexity();
     let complexity_parameters = complexity.complexity_parameters(mazeGrid);
-    return complexity.calculate_complexity(complexity_parameters).toFixed(2);
+    return complexity.calculate_complexity(complexity_parameters);//uwaga tutaj
 
   }
 
@@ -68,27 +81,44 @@ class Solution_demo {
   }
 
   getData() {
-    let outputArray = [];
-    // let mazeType = ["Binary", "Aldous", "Backtracker"];
-    let mazeType = ["Backtracker"];
+    let outputArray =[];
+     let mazeType = ["Binary", "Aldous", "Backtracker"];
+     let solver = ["Dijkstra", "BFS"];
     let mazeSizeX = 0;
     let mazeSizeY = 0;
     let mazeComplexity = 0;
-    let solutionTime = 0;
-    let min = 4;
-    let max = 4;
-
+    let solutionTimeDijkstra = 0;
+    let solutionTimeBFS = 0;
+    let min = 6;
+    let max =  18;
+    let table_header = ["Maze Type", "Maze Size", "Corridors Ratio","Forks Ratio", "Intersections Ratio", "Solution Time Dijkstra", "Solution Time BFS"].join(";");
+    outputArray.push(table_header);
     mazeType.forEach(mazeType => {
       for (let i = 0; i < 1; i++) {
         mazeSizeX = this.getRandomInt(min, max);
         mazeSizeY = this.getRandomInt(min, max);
-        let distance_grid = this.generateMaze(mazeSizeX, mazeSizeY, mazeType);
-        solutionTime = this.generateDijkstraSolution(distance_grid);
+        const distance_grid = this.generateMaze(mazeSizeX, mazeSizeY, mazeType);
+       const copyOf_distance_grid = {...distance_grid};
+     
+        
+        //console.log("HELLO"+ distance_grid.toString());
+        //console.log(distance_grid.grid);
+        //console.log(copyOf_distance_grid.grid);
+     
+        solutionTimeDijkstra = this.generateDijkstraSolution(distance_grid);
+        console.log("Dijkstra Solution PO")
+        console.log(distance_grid.toString());
+        console.log("BFS Solution")
+        solutionTimeBFS = this.generateBfsSolution(distance_grid);
+        console.log(distance_grid.toString());
+       
         mazeComplexity = this.calculateComplexity(distance_grid);
-
-        let outputLine = this.createOutputLine(mazeType, mazeSizeX, mazeSizeY, mazeComplexity, solutionTime).join(";");
+        let corridors_ratio = mazeComplexity[0];
+        let forks_ratio = mazeComplexity[1];
+        let intersections_ratio = mazeComplexity[2];
+        let outputLine = this.createOutputLine(mazeType, mazeSizeX, mazeSizeY, corridors_ratio, forks_ratio, intersections_ratio, solutionTimeDijkstra, solutionTimeBFS).join(";");
         outputArray.push(outputLine);
-        // console.log(distance_grid.toString());    
+          
       }
 
     });
@@ -103,15 +133,18 @@ class Solution_demo {
     })
     // return console.log(outputArray);
   }
-  createOutputLine(mazeType, mazeSizeX, mazeSizeY, mazeComplexity, solutionTime) {
+  createOutputLine(mazeType, mazeSizeX, mazeSizeY, corridors_ratio, forks_ratio, intersections_ratio, solutionTime, solver ) {
     let outputLine = [];
     outputLine.push(mazeType)
     outputLine.push(mazeSizeX * mazeSizeY);
-    outputLine.push(mazeComplexity);
+    outputLine.push(corridors_ratio.toFixed(2));
+    outputLine.push(forks_ratio.toFixed(2));
+    outputLine.push(intersections_ratio.toFixed(2));
     outputLine.push(solutionTime);
+    outputLine.push(solver);
     return outputLine;
   }
 };
 
-new Solution_demo().testMethod()
+new Solution_demo().getData();
 
